df6c7a60fadfab656e94e4c89717e7e7772321c5
Zakia-Fuse <110482892+Zakia-Fuse@users.noreply.github.com>
2022-08-09 17:50:15 +0000

    lesson2

---
+++ main.js
@@ -0,0 +1,21 @@
+//logical operators
+if(3==3 && 4==4){
+    document.write("the condition is true")
+}
+ if(3==3 && 4==4){
+  document.write("write stetement is false and will not printed")
+ }
+ const age =21
+ if (age>18){
+    alert("old enough to registre");
+ }else{
+    alert("user is old enough to enter")
+ }
+ const x =15; 
+ if(x<10)
+ { document.getElementById("demo").innerHTML="x is less than 10";
+ }else if(x>10){
+    document.getElementById("domo2").innerHTML="x is greater than 10";
+ }else{
+    document.getElementById("demo2").innerHTML="x is 10, it is neither greater or less"
+ }
